149da414e3d603389e6dc17f50620578a4228c8b
jakuboklesinski <83898938+jakuboklesinski@users.noreply.github.com>
2021-06-11 00:13:24 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿
+// czyszczenie kodu
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int spr_max(int a, int b, int c)
+{
+	int maximum;
+
+	if ((a > b) && (a > c)) maximum = a; //optymalizacja części kodu 
+	if ((b > a) && (b > c)) maximum = b; //mniejsza ilość linii powoduje większą czytelność
+	if ((c > a) && (c > b)) maximum = c;
+
+	return maximum;
+
+}
+int spr_min(int a, int b, int c)
+{
+	int minimum;
+
+	//optymalizacja kodu 
+	if ((a < b) && (a < c)) minimum = a;
+	if ((b < a) && (b < c)) minimum = b;
+	if ((c < a) && (c < b)) minimum = c;
+
+
+	return minimum;
+
+}
+
+bool positive(int x) {
+
+	if (x >= 0)
+		return true;
+	else
+		return false;
+}
+
+
+
+
+int main()
+{
+	cout << spr_max(-1, 8, 1) << endl;
+	cout << spr_min(-20, -40, -30) << endl; // sprawdzenie poprawności programu
+	cout << positive(5) << endl;// sprawdzenie poprawności programu
+	return 0;
+}
